eca4e196992a618d84804902c618dee22559e745
Petr Vanc <petrvancjr@gmail.com>
2025-01-21 08:38:41 +0100

    fixes

--- naive_merger/naive_merger/HriCommand.py
+++ naive_merger/naive_merger/HriCommand.py
@@ -18,19 +18,36 @@ class HriCommand():
         self.pv_dict = pv_dict
         self.results_dict = self.apply_thresholding()
 
+    
+    @property
+    def target_action(self):
+        return self.pv_dict["action"].max
+
+    @property
+    def target_object(self):
+        return self.pv_dict["object"].max
+
+
     def __matmul__(self, other):
         """ Do modality merging of two HriCommand objects as (hri_command1 @ hri_command2) """
         assert self.arity_names == other.arity_names
         for arity_type in self.arity_names:
-            assert (self.pv_dict[arity_type].names == other.pv_dict[arity_type].names).all(), f"{self.pv_dict[arity_type]} != {other.pv_dict[arity_type]}"
+            assert (self.pv_dict[arity_type].names == other.pv_dict[arity_type].names).all(), f'{self.pv_dict[arity_type]} != {other.pv_dict[arity_type]}'
 
         data_dict_new = {}
-        data_dict = merge_probabilities(self.data_dict, other.data_dict, thresholding=THRESHOLDING)
+        data_dict = merge_probabilities(self.probs_dict, other.probs_dict, thresholding=THRESHOLDING)
         for arity_type in self.arity_names:
-            data_dict_new[arity_type+"_probs"] = data_dict[arity_type]
-            data_dict_new[arity_type] = self.pv_dict[arity_type].names
+            data_dict_new[arity_type+"_probs"] = data_dict[arity_type].p
+            data_dict_new[arity_type+"_names"] = self.pv_dict[arity_type].names
 
         return HriCommand.from_dict(self.arity_names, data_dict_new, thresholding=THRESHOLDING)
+
+    @property
+    def probs_dict(self): # {"action": [...], "object": [...]} instead of {"action_probs": [...], "object_probs": [...]}
+        r = {}
+        for arity in self.arity_names:
+            r[arity] = self.pv_dict[arity].p
+        return r
 
     def apply_thresholding(self):
         targets_dict = {}
@@ -45,18 +62,19 @@ class HriCommand():
             ret[arity_type] = self.pv_dict[arity_type].p
         return ret
     
+
     @classmethod
     def from_ros(cls, msg, thresholding="entropy"):
         msg_dict = json.loads(msg.data[0])
 
-        arity_names = []
+        arity_names = [] # just add arity_names to the HRICommand definition!
         for k in msg_dict.keys():
             if "_probs" in k:
                 arity_names.append(k.split("_probs")[0])
 
         pv_dict = {}
         for arity_type in arity_names:
-            names = msg_dict[arity_type]
+            names = msg_dict[arity_type+"_names"]
             probs = msg_dict[arity_type+"_probs"]
 
             if thresholding == "no thresholding":
@@ -73,7 +91,7 @@ class HriCommand():
     def from_dict(cls, arity_names, data_dict, thresholding):
         pv_dict = {}
         for arity_type in arity_names:
-            names = data_dict[arity_type]
+            names = data_dict[arity_type+"_names"]
             probs = data_dict[arity_type+"_probs"]
 
             if thresholding == "no thresholding":
